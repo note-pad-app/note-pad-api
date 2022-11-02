984fd4d3dcf9826f8c8a1dede153347176f2c5d4
EdrissAria <edrissalokozay77@gmail.com>
2022-11-02 09:54:29 -0700

    working on email varification

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,6 +1,22 @@
+import Mail from '@ioc:Adonis/Addons/Mail'
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class UsersController {
+  public async register({request, response}: HttpContextContract){
+    try{
+      await Mail.send((message) => {
+        message
+          .from('info@example.com')
+          .to(request.input('email'))
+          .subject('Welcome Onboard!')
+          .htmlView('emails/varification', { name: request.input('username') })
+      })
+        response.send('success')
+    }catch(e){
+      response.send('failed')
+    }
+  }
+
   public async index({}: HttpContextContract) {}
 
   public async create({}: HttpContextContract) {}
