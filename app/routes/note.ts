8c14765314c725993f70cd6b513e766e48c44392
EdrissAria <edrissalokozay77@gmail.com>
2024-03-06 06:25:49 -0800

    debug routes

--- app/routes/note.ts
+++ app/routes/note.ts
@@ -2,12 +2,12 @@ import router from '@adonisjs/core/services/router'
 import { middleware } from '#start/kernel';
 
 router.group(() => {
-    router.resource('notes', '#controllers/note_controller')
+    router.resource('notes', '#controllers/notes_controller')
         .apiOnly()
-    router.patch("notes/:id/soft_delete", "#controllers/note_controller.softDelete");
-    router.patch("notes/:id/recover", "#controllers/note_controller.recovery");
-    router.patch("notes/:id/favorite", "#controllers/note_controller.markAsFavorite");
-    router.get("notes/search", "#controllers/note_controller.search")
+    router.patch("notes/:id/soft_delete", "#controllers/notes_controller.softDelete");
+    router.patch("notes/:id/recover", "#controllers/notes_controller.recovery");
+    router.patch("notes/:id/favorite", "#controllers/notes_controller.markAsFavorite");
+    router.get("notes/search", "#controllers/notes_controller.search")
 })
     .use(middleware.auth())
     .prefix("api")
